4fd871e6107170995601673c14d601edbc045543
ImArnav19 <morearnav019@gmail.com>
2024-08-12 22:45:07 +0530

    change 03

--- DevTest/settings.py
+++ DevTest/settings.py
@@ -138,6 +138,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
