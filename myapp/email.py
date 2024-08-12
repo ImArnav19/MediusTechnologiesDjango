c666443b0c42175b227135970dbf76c7e9c3b786
ImArnav19 <morearnav019@gmail.com>
2024-08-12 21:39:45 +0530

    Changes email

--- myapp/email.py
+++ myapp/email.py
@@ -5,6 +5,6 @@ def send_summary_email(summary_html):
     subject = "Python Assignment - Arnav More"
     message = f"Here is the summary of the uploaded data:<br><br>{summary_html}"
     email_from = settings.EMAIL_HOST_USER
-    recipient_list = ['morearnav019@gmail.com',]
+    recipient_list = ['morearnav019@gmail.com','tech@themedius.ai',]
     send_mail(subject, '', email_from, recipient_list, html_message=message)
 
